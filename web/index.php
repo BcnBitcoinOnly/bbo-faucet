b1fa71ddc170ce49b8fb170dbadb6894f9032da0
Marcel Hernandez <e6990620@gmail.com>
2024-05-23 00:26:56 +0200

    implemented IP-based rate limit

--- web/index.php
+++ web/index.php
@@ -39,14 +39,33 @@ $app->get('/', function (ServerRequestInterface $request, ResponseInterface $res
 });
 
 $app->post('/', function (ServerRequestInterface $request, ResponseInterface $response) use ($twig, $settings) {
+    $redis = new Redis([
+        'host' => $settings['redis_host'],
+        'port' => $settings['redis_port'],
+    ]);
+
+    $ip = $request->getHeaderLine('X-Forwarded-For');
+    $userData = $redis->get("ip:$ip");
+    $setOpts = false === $userData ? ['ex' => $settings['cooldown_time']] : ['keepttl'];
+    $userData = false === $userData ? ['btc' => 0] : json_decode($userData, true);
+
     $form = $request->getParsedBody();
 
     if (!is_array($form) || empty($form['address']) || empty($form['amount'])) {
         return $twig->render($response, 'index.html.twig', ['notification' => ['class' => 'is-danger', 'message' => 'Invalid data']]);
     }
 
+    $amount = (float) $form['amount'];
+    if ($userData['btc'] + $amount > $settings['cooldown_max_btc']) {
+        return $twig->render($response, 'index.html.twig', ['notification' => ['class' => 'is-danger', 'message' => 'Too much collected']]);
+    }
+
     $rpc = new BitcoindRpcClient($settings['bitcoind_rpc_url'], $settings['bitcoind_rpc_user'], $settings['bitcoind_rpc_pass'], $settings['wallet_name']);
-    $txId = $rpc->send($form['address'], (float) $form['amount']);
+    $txId = $rpc->send($form['address'], $amount);
+
+    $userData['btc'] += $amount;
+
+    $redis->set("ip:$ip", json_encode($userData), $setOpts);
 
     $message = null === $settings['mempool_url'] ?
         "Transaction sent: $txId" :
