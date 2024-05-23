8d31aed44dfd1b1447b9d003a7fc73f773610f9e
Marcel Hernandez <e6990620@gmail.com>
2024-05-23 22:00:31 +0200

    IP rate limit v2

---
+++ src/RedisLock.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BBO\Faucet;
+
+use Nyholm\Psr7\Response;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+/**
+ * Per-IP exclusive locking implemented with a single Redis instance.
+ *
+ * @see https://redis.io/docs/latest/develop/use/patterns/distributed-locks/#correct-implementation-with-a-single-instance
+ */
+final readonly class RedisLock implements MiddlewareInterface
+{
+    private const int LOCK_TIMEOUT = 15;
+    private const int LOCK_WAIT_TIME = 100000;
+    private const int LOCK_MAX_RETRIES = 10;
+
+    private const string LOCK_RELEASE_SCRIPT_HASH = '647c65a442733a1aa440f99908d249d13b4d6c4a';
+    private const string LOCK_RELEASE_SCRIPT = <<<LUA
+if redis.call("get",KEYS[1]) == ARGV[1] then
+    return redis.call("del",KEYS[1])
+else
+    return 0
+end
+LUA;
+
+    private \Redis $redis;
+
+    public function __construct(\Redis $redis)
+    {
+        $this->redis = $redis;
+    }
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $ip = $request->getHeaderLine('X-Forwarded-For');
+        $lockValue = base64_encode(random_bytes(24));
+
+        $retries = 0;
+        while (false === $this->redis->set("ip:{$ip}_lock", $lockValue, ['nx', 'ex' => self::LOCK_TIMEOUT]) && $retries < self::LOCK_MAX_RETRIES) {
+            usleep(self::LOCK_WAIT_TIME);
+            ++$retries;
+        }
+
+        if (self::LOCK_MAX_RETRIES === $retries) {
+            return new Response(429, body: 'Too Many Requests');
+        }
+
+        $response = $handler->handle($request);
+
+        try {
+            $this->redis->evalSha(self::LOCK_RELEASE_SCRIPT_HASH, ["ip:{$ip}_lock", $lockValue], 1);
+        } catch (\RedisException) {
+            $this->redis->script('load', self::LOCK_RELEASE_SCRIPT);
+            $this->redis->evalSha(self::LOCK_RELEASE_SCRIPT_HASH, ["ip:{$ip}_lock", $lockValue], 1);
+        }
+
+        return $response;
+    }
+}
